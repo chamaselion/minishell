29eea64f1dd3e2d9161f6d8df0e5ca7a08f5a9cd
Balint Szikora <bszikora@student.42heilbronn.de>
2024-09-10 13:29:36 +0200

    Updated header file

---
+++ minishell.h
@@ -0,0 +1,38 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 16:42:16 by mnaumann          #+#    #+#             */
+/*   Updated: 2024/09/10 13:22:58 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <errno.h>
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <term.h>
+#include <termios.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+// Parsing:
+char *strtok(char *str, const char *delimiters); // https://cplusplus.com/reference/cstring/strtok/
+
+// Input handling:
+int handle_input(char *input);
+
+#endif
