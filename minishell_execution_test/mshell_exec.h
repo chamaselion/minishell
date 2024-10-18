56875adc53331b4e8120cd0f7352650b7c4dae06
Balint Szikora <bszikora@student.42heilbronn.de>
2024-10-18 11:16:19 +0200

    Added token struct and extra libraries for testing

--- minishell_execution_test/mshell_exec.h
+++ minishell_execution_test/mshell_exec.h
@@ -1,9 +1,22 @@
 # ifndef MSHELLEXEC
 # define MSHELLEXEC
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+typedef struct s_token {
+    char *start;
+    int length;
+    int role; //0 = nothing special, 1 = command, 2 = executable, 3 = argument, 4 = string/variable to expand/read, 5 = redirect, 6 = pipe, -1 = error (not found)
+    struct s_token *next;
+    struct s_token *prev;
+} t_token;
 
 typedef struct s_command
 {
@@ -12,8 +25,11 @@ typedef struct s_command
     char **args;
     int arg_count;
     char *output;
+	void *related_to;
+	int	relation_type
     struct s_command *next;
     int append;
+    int priority;
 } t_command;
 
 typedef struct s_shell
@@ -32,4 +48,4 @@ void	ft_unset(t_command *cmd, t_shell *shell);
 void	ft_env(t_command *cmd, t_shell *shell);
 void	ft_exit(t_command *cmd, t_shell *shell);
 
-# endif
+# endif;
