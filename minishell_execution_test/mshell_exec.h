8cee223a149ee02b098a1acbed9690465ab50692
bszikora <chamaselion@gmail.com>
2024-10-22 11:47:47 +0200

    Correcting minor mistakes

--- minishell_execution_test/mshell_exec.h
+++ minishell_execution_test/mshell_exec.h
@@ -10,7 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_token {
+typedef struct s_token
+{
     char *start;
     int length;
     int role; //0 = nothing special, 1 = command, 2 = executable, 3 = argument, 4 = string/variable to expand/read, 5 = redirect, 6 = pipe, -1 = error (not found)
@@ -26,7 +27,7 @@ typedef struct s_command
     int arg_count;
     char *output;
 	void *related_to;
-	int	relation_type
+    int	relation_type;
     struct s_command *next;
     int append;
     int priority;
@@ -48,4 +49,4 @@ void	ft_unset(t_command *cmd, t_shell *shell);
 void	ft_env(t_command *cmd, t_shell *shell);
 void	ft_exit(t_command *cmd, t_shell *shell);
 
-# endif;
+# endif
