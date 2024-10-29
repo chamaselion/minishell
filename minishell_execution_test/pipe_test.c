1c8483194162662f229f50105d8d275d5e7dbfad
Balint Szikora <bszikora@student.42heilbronn.de>
2024-10-29 16:17:35 +0100

    Added correct redirection setup and changed test cases

--- minishell_execution_test/pipe_test.c
+++ minishell_execution_test/pipe_test.c
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:47:00 by bszikora          #+#    #+#             */
-/*   Updated: 2024/10/28 12:27:56 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:50:08 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -58,76 +58,89 @@ char *search_command(const char *command)
 	return (free(path), NULL);
 }
 
-void setup_redirection(t_command *cmd, int in_fd, int pipefd[2])
+void handle_input_redirection(t_command *cmd)
 {
     int fd;
-	
     if (cmd->input_redirection)
-	{
+    {
         fd = open(cmd->input_redirection->start, O_RDONLY);
         if (fd == -1)
-		{
+        {
             perror("open input redirection");
             exit(EXIT_FAILURE);
         }
         if (dup2(fd, STDIN_FILENO) == -1)
-		{
+        {
             perror("dup2 input redirection");
             exit(EXIT_FAILURE);
         }
         close(fd);
     }
-	else if (in_fd != 0)
-	{
-        dup2(in_fd, STDIN_FILENO);
-        close(in_fd);
-    }
+}
+
+void handle_output_redirection(t_command *cmd)
+{
+    int fd;
     if (cmd->output_redirection)
-	{
+    {
         fd = open(cmd->output_redirection->start, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (fd == -1)
-		{
+        {
             perror("open output redirection");
             exit(EXIT_FAILURE);
         }
         if (dup2(fd, STDOUT_FILENO) == -1)
-		{
+        {
             perror("dup2 output redirection");
             exit(EXIT_FAILURE);
         }
         close(fd);
     }
-	else if (cmd->append_redirection)
-	{
+}
+
+void handle_append_redirection(t_command *cmd)
+{
+    int fd;
+    if (cmd->append_redirection)
+    {
         fd = open(cmd->append_redirection->start, O_WRONLY | O_CREAT | O_APPEND, 0644);
         if (fd == -1)
-		{
+        {
             perror("open append redirection");
             exit(EXIT_FAILURE);
         }
         if (dup2(fd, STDOUT_FILENO) == -1)
-		{
+        {
             perror("dup2 append redirection");
             exit(EXIT_FAILURE);
         }
         close(fd);
     }
-	else if (cmd->related_to != NULL && cmd->relation_type == 6)
-	{
+}
+
+void handle_pipe_redirection(t_command *cmd, int pipefd[2])
+{
+    if (cmd->related_to != NULL && cmd->relation_type == 6)
+    {
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO);
         close(pipefd[1]);
     }
+}
+
+void handle_heredoc_redirection(t_command *cmd)
+{
+    int fd;
     if (cmd->heredoc_redirection)
-	{
+    {
         fd = open(cmd->heredoc_redirection->start, O_RDONLY);
         if (fd == -1)
-		{
+        {
             perror("open heredoc redirection");
             exit(EXIT_FAILURE);
         }
         if (dup2(fd, STDIN_FILENO) == -1)
-		{
+        {
             perror("dup2 heredoc redirection");
             exit(EXIT_FAILURE);
         }
@@ -135,11 +148,27 @@ void setup_redirection(t_command *cmd, int in_fd, int pipefd[2])
     }
 }
 
+void setup_redirection(t_command *cmd, int in_fd, int pipefd[2])
+{
+    handle_input_redirection(cmd);
+    if (!cmd->input_redirection && in_fd != 0)
+    {
+        dup2(in_fd, STDIN_FILENO);
+        close(in_fd);
+    }
+    handle_output_redirection(cmd);
+    handle_append_redirection(cmd);
+    handle_pipe_redirection(cmd, pipefd);
+    handle_heredoc_redirection(cmd);
+}
+
 char **construct_exec_args(t_command *cmd)
 {
     char **exec_args;
 	int i;
+	int c;
 
+	c = 0;
 	i = 0;
 	exec_args = malloc((cmd->arg_count + 2) * sizeof(char *));
     if (!exec_args)
@@ -148,9 +177,10 @@ char **construct_exec_args(t_command *cmd)
         exit(EXIT_FAILURE);
     }
     exec_args[0] = cmd->command;
-    for (int i = 0; i < cmd->arg_count; i++)
+    while (c < cmd->arg_count)
     {
-        exec_args[i + 1] = cmd->args[i];
+        exec_args[c + 1] = cmd->args[c];
+		c++;
     }
     exec_args[cmd->arg_count + 1] = NULL;
     return exec_args;
@@ -251,110 +281,51 @@ void handle_pipes(t_command *cmd)
         cmd = cmd->related_to;
     }
 }
-
+/*
 int main()
 {
-    // Input redirection test
-    t_command cmd1;
-    t_token input_token;
-    input_token.start = "input.txt";
-    input_token.length = strlen(input_token.start);
-    input_token.role = 5; // Assuming 5 is the role for redirection
-    input_token.next = NULL;
-    input_token.prev = NULL;
+    // Create the command structs
+    t_command cmd1 = {0};
+    t_command cmd2 = {0};
+    t_token token;
 
-    cmd1.command = "cat";
-    cmd1.args = (char *[]){NULL};
-    cmd1.arg_count = 0;
-    cmd1.input = NULL;
-    cmd1.output = NULL;
-    cmd1.related_to = NULL;
-    cmd1.relation_type = 5;
-    cmd1.next = NULL;
-    cmd1.append = 0;
+    token.start = "test.txt";
+	token.length = 8;
+	token.role = 0;
+	token.next = NULL;
+	token.prev = NULL;
+	
+    cmd1.command = "echo";
+	cmd1.input = NULL;
+    cmd1.args = (char*[]){"Hello, world", NULL};
+    cmd1.arg_count = 1;
+	cmd1.output = NULL;
+	cmd1.related_to = &cmd2;
+	cmd1.relation_type = 6;
+    cmd1.next = &cmd2;
     cmd1.is_internal = 0;
-    cmd1.input_redirection = &input_token;
-    cmd1.output_redirection = NULL;
-    cmd1.append_redirection = NULL;
-    cmd1.heredoc_redirection = NULL;
+	cmd1.input_redirection = NULL;
+	cmd1.output_redirection = NULL;
+	cmd1.append_redirection = NULL;
+	cmd1.heredoc_redirection = NULL;
 
-    // Output redirection test
-    t_command cmd2;
-    t_token output_token;
-    output_token.start = "output.txt";
-    output_token.length = strlen(output_token.start);
-    output_token.role = 5; // Assuming 5 is the role for redirection
-    output_token.next = NULL;
-    output_token.prev = NULL;
-
-    cmd2.command = "echo";
-    cmd2.args = (char *[]){"Hello, World!", NULL};
+    cmd2.command = "wc";
+	cmd2.input = NULL;
+    cmd2.args = (char*[]){"-l", NULL};
     cmd2.arg_count = 1;
-    cmd2.input = NULL;
-    cmd2.output = NULL;
-    cmd2.related_to = NULL;
-    cmd2.relation_type = 5;
+	cmd2.output = NULL;
+	cmd2.related_to = NULL;
+	cmd2.relation_type = 5;
     cmd2.next = NULL;
-    cmd2.append = 0;
     cmd2.is_internal = 0;
-    cmd2.input_redirection = NULL;
-    cmd2.output_redirection = &output_token;
-    cmd2.append_redirection = NULL;
-    cmd2.heredoc_redirection = NULL;
-
-    // Append redirection test
-    t_command cmd3;
-    t_token append_token;
-    append_token.start = "append.txt";
-    append_token.length = strlen(append_token.start);
-    append_token.role = 5; // Assuming 5 is the role for redirection
-    append_token.next = NULL;
-    append_token.prev = NULL;
-
-    cmd3.command = "echo";
-    cmd3.args = (char *[]){"Appending text", NULL};
-    cmd3.arg_count = 1;
-    cmd3.input = NULL;
-    cmd3.output = NULL;
-    cmd3.related_to = NULL;
-    cmd3.relation_type = 5;
-    cmd3.next = NULL;
-    cmd3.append = 1;
-    cmd3.is_internal = 0;
-    cmd3.input_redirection = NULL;
-    cmd3.output_redirection = NULL;
-    cmd3.append_redirection = &append_token;
-    cmd3.heredoc_redirection = NULL;
-
-    // Heredoc redirection test
-    t_command cmd4;
-    t_token heredoc_token;
-    heredoc_token.start = "heredoc.txt"; // Assuming heredoc content is stored in a file
-    heredoc_token.length = strlen(heredoc_token.start);
-    heredoc_token.role = 5; // Assuming 5 is the role for redirection
-    heredoc_token.next = NULL;
-    heredoc_token.prev = NULL;
-
-    cmd4.command = "cat";
-    cmd4.args = (char *[]){NULL};
-    cmd4.arg_count = 0;
-    cmd4.input = NULL;
-    cmd4.output = NULL;
-    cmd4.related_to = NULL;
-    cmd4.relation_type = 5;
-    cmd4.next = NULL;
-    cmd4.append = 0;
-    cmd4.is_internal = 0;
-    cmd4.input_redirection = NULL;
-    cmd4.output_redirection = NULL;
-    cmd4.append_redirection = NULL;
-    cmd4.heredoc_redirection = &heredoc_token;
+	cmd2.input_redirection = NULL;
+	cmd2.output_redirection = &token;
+	cmd2.append_redirection = NULL;
+	cmd2.heredoc_redirection = NULL;
 
     // Execute commands
     handle_pipes(&cmd1);
-    handle_pipes(&cmd2);
-    handle_pipes(&cmd3);
-    handle_pipes(&cmd4);
 
     return 0;
 }
+*/
