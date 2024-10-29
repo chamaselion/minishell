ce44edba0da8c6469b98ff287d3fd32d70188485
bszikora <chamaselion@gmail.com>
2024-10-29 16:14:16 +0100

    Changed command struct from void pointer, changed role numbers

--- minishell_execution_test/mshell_exec.h
+++ minishell_execution_test/mshell_exec.h
@@ -12,9 +12,9 @@
 
 typedef struct s_token
 {
-    char *start;
-    int length;
-    int role; //0 = nothing special, 1 = command, 2 = executable, 3 = argument, 4 = string/variable to expand/read, 5 = redirect, 6 = pipe, -1 = error (not found)
+    char *start; //The string containing the token
+    int length; //Length of the string containing the token
+    int role; //0 = delimeter, 1 = command, 2 = executable, 3 = argument, 4 = Assignment operator, 5 = redirect, 6 = pipe, -1 = error (not found)
     struct s_token *next;
     struct s_token *prev;
 } t_token;
@@ -26,11 +26,14 @@ typedef struct s_command
     char **args;
     int arg_count;
     char *output;
-	void *related_to;
-    int	relation_type;
+	struct s_command *related_to;
+    int	relation_type; //0 = delimeter, 1 = command, 2 = executable, 3 = argument, 4 = Assignment operator, 5 = redirect, 6 = pipe, -1 = error (not found)
     struct s_command *next;
-    int append;
-    int priority;
+    int is_internal; // 0 = Not internal, 1 =  Is internal function
+	t_token	*input_redirection;
+    t_token	*output_redirection;
+    t_token	*append_redirection;
+    t_token	*heredoc_redirection;
 } t_command;
 
 typedef struct s_shell
@@ -41,12 +44,21 @@ typedef struct s_shell
 
 typedef void (*func_ptr)(t_command *, t_shell *);
 
-void	ft_echo(t_command *cmd, t_shell *shell);
-void	ft_cd(t_command *cmd, t_shell *shell);
-void	ft_pwd(t_command *cmd, t_shell *shell);
-void	ft_export(t_command *cmd, t_shell *shell);
-void	ft_unset(t_command *cmd, t_shell *shell);
-void	ft_env(t_command *cmd, t_shell *shell);
-void	ft_exit(t_command *cmd, t_shell *shell);
+//void	ft_echo(t_command *cmd, t_shell *shell);
+//void	ft_cd(t_command *cmd, t_shell *shell);
+//void	ft_pwd(t_command *cmd, t_shell *shell);
+//void	ft_export(t_command *cmd, t_shell *shell);
+//void	ft_unset(t_command *cmd, t_shell *shell);
+//void	ft_env(t_command *cmd, t_shell *shell);
+//void	ft_exit(t_command *cmd, t_shell *shell);
+
+
+char	*ft_cd(t_command *cmd);
+char	*ft_echo(t_command *cmd);
+char	*ft_exit(t_command *cmd);
+char	*ft_export(t_command *cmd);
+char	*ft_pwd(t_command *cmd);
+void handle_pipes(t_command *cmd);
+
 
 # endif
