0f71e9f688e447a723b6fd1b0ef866b1fb63163a
Balint Szikora <bszikora@student.42heilbronn.de>
2025-01-20 19:31:57 +0100

    Implementing from tester branch ft functions

---
+++ minishell_ft_env.c
@@ -0,0 +1,32 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_ft_env.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 15:02:05 by bszikora          #+#    #+#             */
+/*   Updated: 2025/01/20 18:39:56 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "minishell.h"
+
+void	ft_env(t_command *cmd)
+{
+	t_env_var *current;
+
+	current = cmd->shell->env_vars;
+	
+	if (cmd->args[0] == NULL)
+	{
+    	while (current)
+    	{
+        	if (current->value)
+            	printf("%s=\"%s\"\n", current->key, current->value);
+        	else
+            	printf("%s\n", current->key);
+        	current = current->next;
+    	}
+    }
+}
