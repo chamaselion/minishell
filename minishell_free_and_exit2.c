9afd733148101f90aa6f21cffe613bb26d658b97
Balint Szikora <bszikora@student.42heilbronn.de>
2025-01-20 19:25:34 +0100

    Corrected and added free functions

--- minishell_free_and_exit2.c
+++ minishell_free_and_exit2.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell_free_and_exit2.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnaumann <mnaumann@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 16:16:20 by mnaumann          #+#    #+#             */
-/*   Updated: 2025/01/02 16:57:04 by mnaumann         ###   ########.fr       */
+/*   Updated: 2025/01/20 16:14:19 by bszikora         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "minishell.h"
 
@@ -23,4 +23,33 @@ void	free_raw_tokens(t_raw_token *first_token)
 		free(temp->segment);
 		free(temp);
 	}
+}
+
+void free_env_vars(t_env_var *env_vars)
+{
+    t_env_var *current;
+    t_env_var *next;
+
+    current = env_vars;
+    while (current)
+    {
+        next = current->next;
+        free(current->string);
+        free(current->key);
+        free(current->value);
+        free(current);
+        current = next;
+    }
+}
+
+void free_shell(t_shell *shell)
+{
+    if (!shell)
+        return;
+
+    if (shell->commands)
+        free_commands(shell->commands);
+    
+    if (shell->env_vars)
+        free_env_vars(shell->env_vars);
 }
