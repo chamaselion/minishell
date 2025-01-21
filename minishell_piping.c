716782ec14e07bc009961205527799764cea417e
Balint Szikora <bszikora@student.42heilbronn.de>
2025-01-21 20:08:42 +0100

    Added exit status saving

--- minishell_piping.c
+++ minishell_piping.c
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:26:56 by bszikora          #+#    #+#             */
-/*   Updated: 2025/01/20 19:03:27 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/01/21 14:17:51 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -51,13 +51,13 @@ void handle_child_process(t_command *cmd, int in_fd, int pipefd[2])
     char **exec_args;
     if (cmd == NULL)
     {
-        fprintf(stderr, "Error: cmd is NULL in handle_child_process\n");
+        ft_putstr_fd("Error, command is NULL in handle_child_process", STDERR_FILENO);
         exit(EXIT_FAILURE);
     }
     exec_args = construct_exec_args(cmd);
     if (exec_args == NULL)
     {
-        fprintf(stderr, "Error: Failed to construct exec args\n");
+        ft_putstr_fd("Error, failed to construct exec args", STDERR_FILENO);
         exit(EXIT_FAILURE);
     }
     setup_redirection(cmd, in_fd, pipefd);
@@ -88,11 +88,26 @@ void handle_child_process(t_command *cmd, int in_fd, int pipefd[2])
         execute_command(cmd, exec_args);
 }*/
 
+void update_exit_code(t_shell *shell, int exit_code)
+{
+    shell->last_exit_code = exit_code;
+}
+
+char *get_exit_code_str(t_shell *shell)
+{
+    char *str;
+    
+    str = ft_itoa(shell->last_exit_code);
+    if (!str)
+        return (NULL);
+    return (str);
+}
+
 void handle_parent_process(t_command *cmd, int *in_fd, int pipefd[2])
 {
     if (cmd == NULL)
     {
-        fprintf(stderr, "Error: cmd is NULL in handle_parent_process\n");
+        ft_putstr_fd("Error, command is NULL in handle_parent_process", STDERR_FILENO);
         return;
     }
     int status;
@@ -108,6 +123,10 @@ void handle_parent_process(t_command *cmd, int *in_fd, int pipefd[2])
         *in_fd = 0;
     }
     waitpid(-1, &status, 0);
+	if (WIFEXITED(status))
+        update_exit_code(cmd->shell, WEXITSTATUS(status));
+    else if (WIFSIGNALED(status))
+        update_exit_code(cmd->shell, 128 + WTERMSIG(status));
 }
 
 void execute_builtin(t_command *cmd)
