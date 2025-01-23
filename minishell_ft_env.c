a4ec1aef8a5328916004036a59a018b6f6da0436
szb <chamaselion@gmail.com>
2025-01-24 00:50:38 +0100

    formatting for norminette

--- minishell_ft_env.c
+++ minishell_ft_env.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell_ft_env.c                                 :+:      :+:    :+:   */
@@ -6,29 +6,29 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 15:02:05 by bszikora          #+#    #+#             */
-/*   Updated: 2025/01/21 19:54:37 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/01/23 22:50:17 by bszikora         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "minishell.h"
 
 int	ft_env(t_command *cmd)
 {
-	t_env_var *current;
+	t_env_var	*current;
 
 	current = cmd->shell->env_vars;
 	if (!cmd)
 		return (1);
 	if (cmd->args[0] == NULL)
 	{
-    	while (current)
-    	{
-        	if (current->value)
-            	printf("%s=\"%s\"\n", current->key, current->value);
-        	else
-            	printf("%s\n", current->key);
-        	current = current->next;
-    	}
-    }
+		while (current)
+		{
+			if (current->value)
+				printf("%s=\"%s\"\n", current->key, current->value);
+			else
+				printf("%s\n", current->key);
+			current = current->next;
+		}
+	}
 	return (0);
 }
