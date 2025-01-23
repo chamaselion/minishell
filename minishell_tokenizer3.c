7fa1f05412652adc417f57d9edb60c5af868f7e1
szb <chamaselion@gmail.com>
2025-01-24 00:55:15 +0100

    formatting for norminette

--- minishell_tokenizer3.c
+++ minishell_tokenizer3.c
@@ -68,94 +68,95 @@ t_raw_token	*handle_double_quote_mark(int *pos)
 	return (token);
 }
 
-t_raw_token *handle_non_quote_segment(const char **input, int *pos)
+t_raw_token	*handle_non_quote_segment(const char **input, int *pos)
 {
-    const char *start = *input;
-    char *segment;
-    t_raw_token *token;
-    //int in_quotes = 0;
-    //char quote_char = 0;
+	const char	*start;
+	char		*segment;
+	t_raw_token	*token;
+	// int in_quotes = 0;
+	// char quote_char = 0;
 
-    while (**input && (!is_whitespace(**input)))
-    {
-        (*input)++;
-    }
-    if (*input == start)
-        return NULL;
-    segment = ft_strndup(start, *input - start);
-    token = create_raw_token(segment, NO_QUOTE, *pos);
-    free(segment);
-    return token;
+	start = *input;
+	while (**input && (!is_whitespace(**input)))
+	{
+		(*input)++;
+	}
+	if (*input == start)
+		return (NULL);
+	segment = ft_strndup(start, *input - start);
+	token = create_raw_token(segment, NO_QUOTE, *pos);
+	free(segment);
+	return (token);
 }
 
 /* t_raw_token *handle_non_quote_segment(const char **input, int *pos)
 {
-    const char *start = *input;
-    char *segment;
-    t_raw_token *token;
-    int in_quotes = 0;
-    char quote_char = 0;
-    char var_name[256];
-    char *write_ptr;
+	const char *start = *input;
+	char *segment;
+	t_raw_token *token;
+	int in_quotes = 0;
+	char quote_char = 0;
+	char var_name[256];
+	char *write_ptr;
 
-    segment = malloc(MAX_PATH);
-    if (!segment)
-        return NULL;
-    write_ptr = segment;
+	segment = malloc(MAX_PATH);
+	if (!segment)
+		return (NULL);
+	write_ptr = segment;
 
-    while (**input && (!is_whitespace(**input) || in_quotes))
-    {
-        if (**input == '=' && !in_quotes)
-        {
-            (*input)++;
-            while (**input && !is_whitespace(**input))
-            {
-                if (**input == '"' || **input == '\'')
-                {
-                    quote_char = **input;
-                    (*input)++;
-                    while (**input && **input != quote_char)
-                        (*input)++;
-                    if (**input == quote_char)
-                        (*input)++;
-                }
-                else if (**input == '$')
-                {
-                    (*input)++;
-                    *input = extract_variable_name(*input, var_name);
-                    append_variable_value(var_name, &write_ptr);
-                }
-                else
-                {
-                    *write_ptr++ = **input;
-                    (*input)++;
-                }
-            }
-            break;
-        }
-        else if (**input == '"' || **input == '\'')
-            break;
-        else if (**input == '$')
-        {
-            (*input)++;
-            *input = extract_variable_name(*input, var_name);
-            append_variable_value(var_name, &write_ptr);
-        }
-        else
-        {
-            *write_ptr++ = **input;
-            (*input)++;
-        }
-    }
-    if (*input == start)
-    {
-        free(segment);
-        return NULL;
-    }
-    *write_ptr = '\0';
-    token = create_raw_token(segment, NO_QUOTE, *pos);
-    free(segment);
-    return token;
+	while (**input && (!is_whitespace(**input) || in_quotes))
+	{
+		if (**input == '=' && !in_quotes)
+		{
+			(*input)++;
+			while (**input && !is_whitespace(**input))
+			{
+				if (**input == '"' || **input == '\'')
+				{
+					quote_char = **input;
+					(*input)++;
+					while (**input && **input != quote_char)
+						(*input)++;
+					if (**input == quote_char)
+						(*input)++;
+				}
+				else if (**input == '$')
+				{
+					(*input)++;
+					*input = extract_variable_name(*input, var_name);
+					append_variable_value(var_name, &write_ptr);
+				}
+				else
+				{
+					*write_ptr++ = **input;
+					(*input)++;
+				}
+			}
+			break ;
+		}
+		else if (**input == '"' || **input == '\'')
+			break ;
+		else if (**input == '$')
+		{
+			(*input)++;
+			*input = extract_variable_name(*input, var_name);
+			append_variable_value(var_name, &write_ptr);
+		}
+		else
+		{
+			*write_ptr++ = **input;
+			(*input)++;
+		}
+	}
+	if (*input == start)
+	{
+		free(segment);
+		return (NULL);
+	}
+	*write_ptr = '\0';
+	token = create_raw_token(segment, NO_QUOTE, *pos);
+	free(segment);
+	return (token);
 }*/
 
 void	print_raw_tokens(t_raw_token *first_token)
@@ -163,11 +164,9 @@ void	print_raw_tokens(t_raw_token *first_token)
 	int count = 0;
 	while (first_token)
 	{
-		printf("Token %d: '%s' (Quote State: %d, Position: %d)\n",
-				count++,
-				first_token->segment,
-				first_token->quote_state,
-				first_token->position);
+		printf("Token %d: '%s' (Quote State: %d, Position: %d)\n", count++,
+			first_token->segment, first_token->quote_state,
+			first_token->position);
 		if (first_token->next == NULL)
 			break ;
 		first_token = first_token->next;
@@ -176,16 +175,16 @@ void	print_raw_tokens(t_raw_token *first_token)
 
 void	print_tokens(t_token *first_token)
 {
-	int count = 0;
-	while (first_token != NULL) {
-    	printf("Token %d: '%s' (Quote State: %d, Position: %d, Command: %d, Role: %d)\n",
-           count++,
-           first_token->content,
-           first_token->quote_state,
-           first_token->position,
-           first_token->command_expected,
-           first_token->role);
-    first_token = first_token->next;
+	int	count;
+
+	count = 0;
+	while (first_token != NULL)
+	{
+		printf("Token %d: '%s' (Quote State: %d, Pos: %d, Command: %d, Role: %d)\n",
+			count++, first_token->content,
+			first_token->quote_state, first_token->position,
+			first_token->command_expected, first_token->role);
+		first_token = first_token->next;
 	}
 	printf("done printing tokens\n");
 }
