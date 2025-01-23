0c5456d1c824e08abda6fd57eb30de653c4e8c3a
szb <chamaselion@gmail.com>
2025-01-24 00:51:19 +0100

    formatting for norminette

--- minishell_ft_export.c
+++ minishell_ft_export.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell_ft_export.c                              :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:28:41 by bszikora          #+#    #+#             */
-/*   Updated: 2025/01/21 22:04:20 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/01/23 23:03:29 by bszikora         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "minishell.h"
 
@@ -62,51 +62,54 @@ int ft_export(t_command *cmd)
     }
     return (0);
 }*/
-
-
-int ft_export(t_command *cmd)
+void	export_print(t_env_var *current)
 {
-    t_env_var *current;
-    int i;
-    char *key;
-    char *value;
-    char *equal_sign;
+	while (current)
+	{
+		if (current->value)
+			printf("declare -x %s=\"%s\"\n", current->key, current->value);
+		else
+			printf("declare -x %s\n", current->key);
+		current = current->next;
+	}
+}
+
+void	handle_env_var_with_value(t_command *cmd, int i, char *equal_sign)
+{
+	char	*key;
+	char	*value;
+
+	key = ft_strndup(cmd->args[i], equal_sign - cmd->args[i]);
+	value = ft_strdup(equal_sign + 1);
+	set_or_create_env_var(&(cmd->shell->env_vars), key, value);
+	free(key);
+	free(value);
+}
+
+int	ft_export(t_command *cmd)
+{
+	t_env_var	*current;
+	int			i;
+	char		*equal_sign;
 
 	i = 0;
-    current = cmd->shell->env_vars;
-
+	current = cmd->shell->env_vars;
 	if (!cmd)
 		return (1);
-    if (cmd->args[0] == NULL)
-    {
-        while (current)
-        {
-            if (current->value)
-                printf("declare -x %s=\"%s\"\n", current->key, current->value);
-            else
-                printf("declare -x %s\n", current->key);
-            current = current->next;
-        }
-    }
-    else
-    {
-        while (cmd->args[i])
-        {
-            equal_sign = strchr(cmd->args[i], '=');
-            if (equal_sign)
-            {
-                key = strndup(cmd->args[i], equal_sign - cmd->args[i]);
-                value = strdup(equal_sign + 1);
-                set_or_create_env_var(&(cmd->shell->env_vars), key, value);
-                free(key);
-                free(value);
-            }
-            else
-            {
-                set_or_create_env_var(&(cmd->shell->env_vars), cmd->args[i], "");
-            }
+	if (cmd->args[0] == NULL)
+		export_print(current);
+	else
+	{
+		while (cmd->args[i])
+		{
+			equal_sign = ft_strchr(cmd->args[i], '=');
+			if (equal_sign)
+				handle_env_var_with_value(cmd, i, equal_sign);
+			else
+				set_or_create_env_var(&(cmd->shell->env_vars), cmd->args[i],
+					"");
 			i++;
-        }
-    }
+		}
+	}
 	return (0);
 }
