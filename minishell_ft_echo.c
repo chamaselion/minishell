917a01c5242b4a844deb6be079e438b1122029b7
szb <chamaselion@gmail.com>
2025-01-24 00:50:15 +0100

    formatting for norminette

--- minishell_ft_echo.c
+++ minishell_ft_echo.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell_ft_echo.c                                :+:      :+:    :+:   */
@@ -6,37 +6,36 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:44:25 by bszikora          #+#    #+#             */
-/*   Updated: 2025/01/21 15:04:38 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/01/23 22:50:10 by bszikora         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-int ft_echo(t_command *cmd)
+int	ft_echo(t_command *cmd)
 {
-    int t_newline;
-    int i;
+	int	t_newline;
+	int	i;
 
 	t_newline = 1;
 	i = 0;
 	if (!cmd)
 		return (1);
-    if (cmd->args[0] && strcmp(cmd->args[0], "-n") == 0)
-    {
-        t_newline = 0;
-        i = 1;
-    }
-    while (cmd->args[i])
-    {
-        printf("%s", cmd->args[i]);
-        if (cmd->args[i + 1])
-            printf(" ");
-        i++;
-    }
-	
-    if (t_newline)
+	if (cmd->args[0] && strcmp(cmd->args[0], "-n") == 0)
 	{
-    	printf("\n");
+		t_newline = 0;
+		i = 1;
+	}
+	while (cmd->args[i])
+	{
+		printf("%s", cmd->args[i]);
+		if (cmd->args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (t_newline)
+	{
+		printf("\n");
 	}
 	return (0);
 }
