632f0b4023552c1fdf234ec7074b7ff76c70028c
szb <chamaselion@gmail.com>
2025-01-24 00:54:45 +0100

    formatting for norminette

--- minishell_syntax_check.c
+++ minishell_syntax_check.c
@@ -2,30 +2,33 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell_syntax_check.c                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mnaumann <mnaumann@student.42heilbronn.    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 19:01:33 by mnaumann          #+#    #+#             */
-/*   Updated: 2024/12/04 15:08:02 by mnaumann         ###   ########.fr       */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2025/01/24 00:18:22 by bszikora          #+#    #+#             */
+/*   Updated: 2025/01/24 00:18:22 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int validate_token_syntax(t_token *token_list)
+int	validate_token_syntax(t_token *token_list)
 {
-    t_token *current;
-	
+	t_token	*current;
+
 	current = token_list;
-    while (current != NULL)
-    {
-        if (current->role == ROLE_PIPE && 
-            (!current->prev || current->prev->role == ROLE_PIPE))
-            return SYNTAX_ERROR;
-        if (current->role == ROLE_REDIRECT && 
-            (!current->next || current->next->role != ROLE_ARGUMENT))
-            return SYNTAX_ERROR;
-        current = current->next;
-    }
-    return SYNTAX_VALID;
+	while (current != NULL)
+	{
+		if (current->role == ROLE_PIPE && (!current->prev
+				|| current->prev->role == ROLE_PIPE))
+			return (SYNTAX_ERROR);
+		if (current->role == ROLE_REDIRECT && (!current->next
+				|| current->next->role != ROLE_ARGUMENT))
+			return (SYNTAX_ERROR);
+		current = current->next;
+	}
+	return (SYNTAX_VALID);
 }
