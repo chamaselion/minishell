19e3e24d2da311d1121708bc2fcdd718d47d1891
szb <chamaselion@gmail.com>
2025-02-01 14:37:54 +0100

    REstorting old version

--- minishell_syntax_check.c
+++ minishell_syntax_check.c
@@ -12,35 +12,19 @@
 
 #include "minishell.h"
 
-
 int	validate_token_syntax(t_token *token_list)
 {
-	t_token *current;
+	t_token	*current;
 
 	current = token_list;
 	while (current != NULL)
 	{
-		if (current->role == ROLE_PIPE && (!current->prev || !current->next
+		if (current->role == ROLE_PIPE && (!current->prev
 				|| current->prev->role == ROLE_PIPE))
-		{
 			return (SYNTAX_ERROR);
-		}
 		if (current->role == ROLE_REDIRECT && (!current->next
-				|| current->next->role == ROLE_REDIRECT
-				|| current->next->role == ROLE_PIPE))
-		{
+				|| current->next->role != ROLE_ARGUMENT))
 			return (SYNTAX_ERROR);
-		}
-		if (current->role == ROLE_REDIRECT && current->next
-			&& current->next->role == ROLE_REDIRECT)
-		{
-			return (SYNTAX_ERROR);
-		}
-		if (current->role == ROLE_PIPE && current->next
-			&& current->next->role == ROLE_REDIRECT)
-		{
-			return (SYNTAX_ERROR);
-		}
 		current = current->next;
 	}
 	return (SYNTAX_VALID);
