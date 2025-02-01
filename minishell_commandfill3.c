4b746ff05a6635b1d02df81642f42134a9e0e97e
szb <chamaselion@gmail.com>
2025-02-01 16:34:37 +0100

    NOrminetting

--- minishell_commandfill3.c
+++ minishell_commandfill3.c
@@ -6,53 +6,24 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:00:49 by bszikora          #+#    #+#             */
-/*   Updated: 2025/02/01 14:03:11 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/02/01 16:31:27 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	remove_last_empty_command(t_command *head_cmd, t_command *current_cmd)
-{
-	t_command	*prev_cmd;
-
-	if (current_cmd->command == NULL && current_cmd->args == NULL)
-	{
-		prev_cmd = head_cmd;
-		while (prev_cmd->next != current_cmd)
-		{
-			prev_cmd = prev_cmd->next;
-		}
-		prev_cmd->next = NULL;
-		free(current_cmd);
-	}
-}
-
-int	update_redirection_last_path(t_token *ct)
+int	handle_redirect_link(t_token *ct, t_command *c_cmd)
 {
 	if (strcmp(ct->content, ">") == 0)
-		return (1);
+		add_redirect(&c_cmd->output_redirections, ct->next);
 	else if (strcmp(ct->content, "<") == 0)
-		return (2);
+		add_redirect(&c_cmd->input_redirections, ct->next);
 	else if (strcmp(ct->content, ">>") == 0)
-		return (3);
+		add_redirect(&c_cmd->append_redirections, ct->next);
 	else if (strcmp(ct->content, "<<") == 0)
-		return (4);
-	return (0);
-}
-
-int	handle_redirect_link(t_token *ct, t_command *current_cmd)
-{
-	if (strcmp(ct->content, ">") == 0)
-		add_redirect(&current_cmd->output_redirections, ct->next);
-	else if (strcmp(ct->content, "<") == 0)
-		add_redirect(&current_cmd->input_redirections, ct->next);
-	else if (strcmp(ct->content, ">>") == 0)
-		add_redirect(&current_cmd->append_redirections, ct->next);
-	else if (strcmp(ct->content, "<<") == 0)
-		add_redirect(&current_cmd->heredoc_redirections, ct->next);
-	if (current_cmd->redir_count < 64)
-        current_cmd->redir_order[current_cmd->redir_count++] = update_redirection_last_path(ct);
+		add_redirect(&c_cmd->heredoc_redirections, ct->next);
+	if (c_cmd->redir_count < 64)
+		c_cmd->redir_order[c_cmd->redir_count++] = update_redir_lp(ct);
 	return (0);
 }
 
@@ -79,19 +50,8 @@ int	handle_redirect_and_update(t_token **ct, t_command *current_cmd,
 	}
 }
 
-int	link_commands_and_tokens(t_token *tokens, t_command *cmd)
+int	process_command_tokens(t_token *ct, t_command *current_cmd, t_command *cmd)
 {
-	t_token		*ct;
-	t_command	*current_cmd;
-
-	if (validate_token_syntax(tokens) == SYNTAX_ERROR)
-	{
-		ft_putstr_fd("Error: syntax error\n", STDERR_FILENO);
-		update_exit_code(cmd->shell, 2);
-		return (1);
-	}
-	ct = tokens;
-	current_cmd = cmd;
 	while (ct)
 	{
 		if (ct->role == 5)
@@ -109,6 +69,21 @@ int	link_commands_and_tokens(t_token *tokens, t_command *cmd)
 		}
 		ct = ct->next;
 	}
+	return (0);
+}
+
+int	link_commands_and_tokens(t_token *tokens, t_command *cmd)
+{
+	t_token		*ct;
+	t_command	*current_cmd;
+
+	if (validate_token_syntax(tokens) == SYNTAX_ERROR)
+		return (ft_putstr_fd("Error: syntax error\n", STDERR_FILENO),
+			update_exit_code(cmd->shell, 2), 1);
+	ct = tokens;
+	current_cmd = cmd;
+	if (process_command_tokens(ct, current_cmd, cmd) == 1)
+		return (1);
 	ft_function_marker(cmd);
 	return (0);
 }
