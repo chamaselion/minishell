d5aad205221e89eccfc9d524570df7f0f4df38f7
Balint Szikora <bszikora@2-B-6.42heilbronn.de>
2025-02-02 20:24:56 +0100

    Added check for validating env var

--- minishell_ft_unset.c
+++ minishell_ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:50:41 by bszikora          #+#    #+#             */
-/*   Updated: 2025/02/01 16:11:28 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/02/02 20:06:22 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,43 @@ static void	remove_env_var(t_env_var **env_vars, t_env_var *var)
 	free(var);
 }
 
+bool	is_valid_unset(const char *str)
+{
+	int	i;
+
+	if (!str || (!ft_isalpha(str[0]) && str[0] != '_'))
+		return (false);
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+int	unset_argument_check(t_command *cmd)
+{
+	int	i;
+
+	i = 0;
+	if (!cmd)
+		return (1);
+	while (cmd->args[i])
+	{
+		if (!is_valid_unset(cmd->args[i]))
+		{
+			ft_putstr_fd("export: '", 2);
+			ft_putstr_fd(cmd->args[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	ft_unset(t_command *cmd)
 {
 	t_env_var	*current;
@@ -34,6 +71,8 @@ int	ft_unset(t_command *cmd)
 	if (!cmd->args[0])
 		return (0);
 	i = 0;
+	if (unset_argument_check(cmd) == 1)
+		return (1);
 	while (cmd->args[i])
 	{
 		current = cmd->shell->env_vars;
